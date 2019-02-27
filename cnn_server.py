297cfc44c8fee6619758115d76c818f9431eae89
vincenzoscotto001 <vincenzo.scottodiuccio@entechconsulting.com>
2019-02-27 13:39:26 -0500

    Fix in cnn_server

--- cnn_server.py
+++ cnn_server.py
@@ -38,7 +38,12 @@ def handledata():
         else:
             prediction = 'cat'
     return render_template('index.html',prediction = prediction.upper())
+    
 
+if __name__ == '__main__':
+    app.debug = True
+    port = int(os.environ.get('POST',5000))
+    app.run(host='0.0.0.0', port = port)
 
 
 
